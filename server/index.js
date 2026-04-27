4a9511ec0ad5b70b687edd3829b21fb7c0d6d91b
Santosh Yadav <santoshgkp107@gmail.com>
2026-04-27 21:32:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,10 +12,11 @@ import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
+const cors = require("cors");
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: "https://your-frontend-name.onrender.com",
+  credentials: true
 }));
 
 
